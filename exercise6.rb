d2313653828c3dac9fcb80a3100fb8bacffdb821
Stella <stellaha4@gmail.com>
2016-06-01 13:41:10 -0400

    defined a method and added rice

--- exercise6.rb
+++ exercise6.rb
@@ -1,16 +1,17 @@
-#make a shopping list
+
+#make a method that can output grocery list easier
 
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
-#output each item on console and have each line start with (*)
+def output_list (recall_list)
+    recall_list.each do |item|
+        puts "* #{item}"
+    end
+end
 
-grocery_list = ["ketchup", "mayo", "relish", "rice"]
-grocery_list.each do |x| puts "* #{x}" end
+p output_list(grocery_list)
 
-
-#add rice and output it again
+#add rice
 grocery_list.push "rice"
-puts "* #{grocery_list[4]}"
 
-
-#make a method that can output grocery list easier
+p output_list(grocery_list)
