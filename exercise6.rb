78e2dce502effefae161e3c46727aa6ae556a2de
Stella <stellaha4@gmail.com>
2016-05-31 23:24:18 -0400

    Created exercise6 and completed step 1

---
+++ exercise6.rb
@@ -0,0 +1,8 @@
+#make a shopping list
+
+shopping_list = ["ketchup", "mayo", "relish"]
+
+#output each item on console and have each line start with (*)
+puts "* #{shopping_list[0]}"
+puts "* #{shopping_list[1]}"
+puts "* #{shopping_list[2]}"
