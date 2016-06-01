1bdb6e7337eb1ca61c9526092fe6790fad58d88b
Stella <stellaha4@gmail.com>
2016-06-01 00:20:21 -0400

    corrected array name and created method

--- exercise6.rb
+++ exercise6.rb
@@ -1,15 +1,27 @@
+=begin
 #make a shopping list
 
-shopping_list = ["carrots", "toilet paper", "apples", "salmon"]
+grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
 #output each item on console and have each line start with (*)
-puts "* #{shopping_list[0]}"
-puts "* #{shopping_list[1]}"
-puts "* #{shopping_list[2]}"
-puts "* #{shopping_list[3]}"
+puts "* #{grocery_list[0]}"
+puts "* #{grocery_list[1]}"
+puts "* #{grocery_list[2]}"
+puts "* #{grocery_list[3]}"
 
 #add rice and output it again
 shopping_list.push "rice"
-puts "* #{shopping_list[4]}"
+puts "* #{grocery_list[4]}"
+=end
 
 #make a method that can output grocery list easier
+def output_grocery_list
+grocery_list = ["carrots", "toilet paper", "apples", "salmon", "rice"]
+puts "* #{grocery_list[0]}"
+puts "* #{grocery_list[1]}"
+puts "* #{grocery_list[2]}"
+puts "* #{grocery_list[3]}"
+puts "* #{grocery_list[4]}"
+end
+
+output_grocery_list
