317336aeb56cec9252fb3c82e55b6b60ffc69726
Stella <stellaha4@gmail.com>
2016-05-31 22:47:41 -0400

    updated comments, added celcius conversion, removed tempurature variable

--- exercise5.rb
+++ exercise5.rb
@@ -3,10 +3,15 @@
 
 print "Please provide the temperature in Fahrenheit: "
 
-#call your method and pass it to the user input as a parameter
+#defined a method called fahrenheit and passed in the user input as a parameter
 def fahrenheit (user_input)
-  puts "the Fahrenheit is #{user_input}"
+#declared a new variable called celcius and assigned value to equal user_input and formula for converting F to C
+  celcius = (user_input - 32) * 5/9
+#use puts to output onto console
+  puts "the conversion of #{user_input} Fahrenheit to Celcius is #{celcius}"
+#return user_input
   return
 end
 
-temperature = fahrenheit (gets.chomp.to_i)
+#called method
+fahrenheit (gets.chomp.to_i)
