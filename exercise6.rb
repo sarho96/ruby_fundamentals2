2df74bf7929fbf850d4d46b439ad5dee49581038
Stella <stellaha4@gmail.com>
2016-06-01 00:17:49 -0400

    changed grocery list items

--- exercise6.rb
+++ exercise6.rb
@@ -1,12 +1,15 @@
 #make a shopping list
 
-shopping_list = ["ketchup", "mayo", "relish"]
+shopping_list = ["carrots", "toilet paper", "apples", "salmon"]
 
 #output each item on console and have each line start with (*)
 puts "* #{shopping_list[0]}"
 puts "* #{shopping_list[1]}"
 puts "* #{shopping_list[2]}"
+puts "* #{shopping_list[3]}"
 
 #add rice and output it again
 shopping_list.push "rice"
-puts "* #{shopping_list[3]}"
+puts "* #{shopping_list[4]}"
+
+#make a method that can output grocery list easier
