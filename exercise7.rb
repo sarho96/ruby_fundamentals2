dd61a75756b51d43b799c827089b5d2053ef85f7
Stella <stellaha4@gmail.com>
2016-06-01 15:10:49 -0400

    outputed only cohort names

--- exercise7.rb
+++ exercise7.rb
@@ -5,18 +5,20 @@ cohort2: 42,
 cohort3: 22
 }
 
-
-
 #create a method that displays the name and number of students for each cohort3
 
 def cohort_list(list) #defined a method called cohort_list and set parameter as (list)
   list.each do |key,value| #for each item on list, do the following:
-  puts "#{key}: #{value}" #for whatever we assign as key and value, put "key: value"
+  puts "#{key}: #{value} students" #for whatever we assign as key and value, put "key: value"
 end
 end
 
 puts cohort_list(students) #output method and assign students into parameter
 
 #add cohort4: 43 students to hash
-students[:cohort4] = 43 #
-puts cohort_list(students)
+students[:cohort4] = 43
+
+#use the keys method to output all the cohort names
+students.each do |key, value|
+  puts "#{key}"
+end
