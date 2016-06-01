814bc73d3671dd7d7e5ea56ce227df120e003fee
Stella <stellaha4@gmail.com>
2016-06-01 14:05:21 -0400

    added if statement to see if I need bananas

--- exercise6.rb
+++ exercise6.rb
@@ -14,8 +14,14 @@ p output_list(grocery_list)
 #add rice
 grocery_list.push "rice"
 
-#test output with new addition to list
-p output_list(grocery_list)
-
 #output grocery list size
 puts grocery_list.size
+
+#Check to see if your list includes "bananas". If it does, output "You need to pick up
+#bananas". Otherwise, output "You don't need to pick up bananas today".
+
+if grocery_list.include?("bananas") == true
+  puts "You need to pick up bananas"
+else
+  puts "You don't need to pick up bananas today"
+end
