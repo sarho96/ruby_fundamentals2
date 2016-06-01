b3b197e3780e7b2c38e84d067f31f63af41c5079
Stella <stellaha4@gmail.com>
2016-06-01 11:30:59 -0400

    revisited step 1 to simplify code

--- exercise6.rb
+++ exercise6.rb
@@ -1,27 +1,16 @@
-=begin
 #make a shopping list
 
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
 #output each item on console and have each line start with (*)
-puts "* #{grocery_list[0]}"
-puts "* #{grocery_list[1]}"
-puts "* #{grocery_list[2]}"
-puts "* #{grocery_list[3]}"
+
+grocery_list = ["ketchup", "mayo", "relish", "rice"]
+grocery_list.each do |x| puts "* #{x}" end
+
 
 #add rice and output it again
-shopping_list.push "rice"
+grocery_list.push "rice"
 puts "* #{grocery_list[4]}"
-=end
+
 
 #make a method that can output grocery list easier
-def output_grocery_list
-grocery_list = ["carrots", "toilet paper", "apples", "salmon", "rice"]
-puts "* #{grocery_list[0]}"
-puts "* #{grocery_list[1]}"
-puts "* #{grocery_list[2]}"
-puts "* #{grocery_list[3]}"
-puts "* #{grocery_list[4]}"
-end
-
-output_grocery_list
